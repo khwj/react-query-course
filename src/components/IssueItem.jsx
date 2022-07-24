c6b1c7a46c5416d296c192a74386e3c0d40801f0
James Khwunchai J <khwunchai.j@ku.th>
2022-07-24 16:14:51 +0000

    Add the first exercise

---
+++ src/components/IssueItem.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import { GoComment, GoIssueClosed, GoIssueOpened } from "react-icons/go";
+import { relativeDate } from "../helpers/relativeDate";
+
+export function IssueItem({
+  id, number, title, status, labels, dueDate, createdDate, createdBy, completedDate, commentCount, assignee
+}) {
+  return (
+    <li>
+      <div>
+        {status === "done" || status === "cancelled" ? (
+          <GoIssueClosed style={{ color: "red" }} />
+        ) : (
+          <GoIssueOpened style={{ color: "green" }} />
+        )}
+      </div>
+      <div className="issue-content">
+        <span>
+          <Link to={`/issue/${number}`}>{title}</Link>
+          {labels.map(label => <span key={label.id} className="label red">
+            {label}
+          </span>)}
+        </span>
+        <small>
+          #{number} opened {relativeDate(createdDate)} by {createdBy}
+        </small>
+      </div>
+      {assignee ? <div>{assignee}</div> : null}
+      <span className="comment-count">
+        {commentCount > 0 ? (
+          <>
+            <GoComment />
+            {commentCount}
+          </>
+        ) : null}
+      </span>
+    </li>
+  );
+}
